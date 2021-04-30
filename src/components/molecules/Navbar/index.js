6f5f96139441c11d2e90e4fa203969b252a8ad39
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 16:36:38 +0800

    adding firebase

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -1,15 +1,15 @@
 function Navbar() {
   return (
     <nav
-      class="navbar navbar-expand-lg navbar-light shadow-sm"
+      className="navbar navbar-expand-lg navbar-light shadow-sm"
       style={{ backgroundColor: "#e3f2fd" }}
     >
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
           Unklab.Fun
         </a>
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -17,12 +17,12 @@ function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Home
               </a>
             </li>
---
+++ src/config/Firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0jHToKn_5i_y0325WxyA01LCFZ1VzWcY",
+  authDomain: "final-project-backend-41ac1.firebaseapp.com",
+  projectId: "final-project-backend-41ac1",
+  storageBucket: "final-project-backend-41ac1.appspot.com",
+  messagingSenderId: "687247481455",
+  appId: "1:687247481455:web:a845ad550972832f5b4b9a",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
