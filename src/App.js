86a9ecb9618502f267e6412ad5ba1202a309c482
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 15:52:57 +0800

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod magnam
+        pariatur sunt eius temporibus maiores quasi ex aspernatur dolore
+        dolorem. At debitis amet corrupti magni. Quod ad quae officia numquam
+        nulla! A quos nam animi, maxime repellendus nostrum et necessitatibus
+        corporis blanditiis iste aut, recusandae sint quibusdam obcaecati
+        voluptates repudiandae veritatis facere, repellat ipsam. Delectus eum
+        neque, ullam nesciunt eveniet optio vitae a natus. Consequatur delectus
+        optio asperiores quia numquam autem aliquid natus odit explicabo nam
+        totam alias quibusdam, impedit consequuntur eos, nisi sint quae? Ducimus
+        consectetur cumque voluptates a laudantium quo eos natus delectus
+        dolore? Expedita vero numquam tempore.
+      </p>
     </div>
   );
 }
